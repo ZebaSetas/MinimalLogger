c8c6e1ed218ebc35af5ae41ee462c4b697bffa57
Sebastian Zawrzykraj <48572502+ZebaSetas@users.noreply.github.com>
2020-06-13 05:20:51 -0300

    Se agregan mejoras, ahora se puede loggear un guid, y fatal

--- Logger/godataLogger.js
+++ Logger/godataLogger.js
@@ -25,11 +25,21 @@ module.exports = class Logger {
     var logPath = extractLogPath(optionalLogPath)
     var nameServerLog = 'server.log'
     var nameExceptionLog = 'exception.log'
+    var customLevels = {
+      levels: {
+        fatal: 0,
+        error: 1,
+        warn: 2,
+        info: 3,
+        debug: 4
+      }
+    }
     var filename = path.join(logPath, nameServerLog)
     var fileLogger = createLogger({
+      levels: customLevels.levels,
       format: format.combine(
         format.simple(), format.timestamp(), format.printf(info =>
-          `[${dateFormat(info.timestamp,'yyyy-mm-dd hh:MM:ss')}][${process.pid}][${info.level}][${filenamePath}]${info.message}`
+          `[${dateFormat(info.timestamp,'yyyy-mm-dd HH:MM:ss')}][${process.pid}][${info.level}][${filenamePath}]${info.message}`
         )),
       transports: [
         new transports.File({
@@ -46,42 +56,83 @@ module.exports = class Logger {
       ]
     })
     var consoleLogger = createLogger({
+      levels: customLevels.levels,
       format: format.combine(
         format.simple(), format.timestamp(), format.printf(info =>
-          `[${dateFormat(info.timestamp,'yyyy-mm-dd hh:MM:ss')}][${process.pid}][${info.level}][${filenamePath}]${info.message}`
+          `[${dateFormat(info.timestamp,'yyyy-mm-dd HH:MM:ss')}][${process.pid}][${info.level}][${filenamePath}]${info.message}`
         )),
       transports: [
         new transports.Console()
       ]
     })
-    var log = (message, initTime, endTime, chalkColor, fileLogger, consoleLogger) => {
+
+    var log = (message, chalkColor, fileLogger, consoleLogger, initTime, endTime, guid) => {
       var separator = ' >>> '
       var messageToLog = ''
-      if (isNaN(initTime)) messageToLog = separator + message
-      else {
-        var diference
+      if (isNaN(initTime)) {
+        messageToLog = buildMessageWithoutTime(initTime, messageToLog, separator, message)
+      } else {
         if (isNaN(endTime)) {
-          var endTime = new Date().getTime()
-          diference = endTime - initTime
-        } else diference = endTime - initTime
-        messageToLog = `[${diference}]${separator}${message}`
-      }
+          messageToLog = buildMessageWithInitTime(initTime, endTime, messageToLog, separator, message)
+        } else {
+          messageToLog = buildMessageWithBothTime(endTime, initTime, guid,
+            messageToLog, separator, message);
+        }
 
+      }
       fileLogger(messageToLog)
       consoleLogger(chalkColor(messageToLog))
     }
 
-    this.info = (message, initTime, endTime) =>
-      log(message, initTime, endTime, chalk.green, fileLogger.info, consoleLogger.info)
+    this.info = (message, initTime, endTime, guid) =>
+      log(message, chalk.green, fileLogger.info, consoleLogger.info, initTime, endTime, guid)
 
-    this.debug = (message, initTimestamp, endTimestamp) =>
-      log(message, initTimestamp, endTimestamp, chalk.green, fileLogger.debug, consoleLogger.debug)
+    this.debug = (message, initTime, endTime, guid) =>
+      log(message, chalk.green, fileLogger.debug, consoleLogger.debug, initTime, endTime, guid)
 
-    this.warn = (message, initTimestamp, endTimestamp) =>
-      log(message, initTimestamp, endTimestamp, chalk.yellow, fileLogger.warn, consoleLogger.warn)
+    this.warn = (message, initTime, endTime, guid) =>
+      log(message, chalk.yellow, fileLogger.warn, consoleLogger.warn, initTime, endTime, guid)
 
-    this.error = (message, initTimestamp, endTimestamp) =>
-      log(message, initTimestamp, endTimestamp, chalk.red, fileLogger.error, consoleLogger.error)
+    this.error = (message, initTime, endTime, guid) =>
+      log(message, chalk.red, fileLogger.error, consoleLogger.error, initTime, endTime, guid)
+
+    this.fatal = (message, initTime, endTime, guid) =>
+      log(message, chalk.bgRed, fileLogger.fatal, consoleLogger.fatal, initTime, endTime, guid)
   }
 
+}
+
+function buildMessageWithBothTime(endTime, initTime, guid, messageToLog,
+  separator, message) {
+  var diference = endTime - initTime;
+  if (typeof guid !== 'undefined' && guid !== null) {
+    var _guid = guid;
+    messageToLog =
+      `[${diference}][Guid:${_guid}]${separator}${message}`;
+  } else
+    messageToLog = `[${diference}]${separator}${message}`;
+  return messageToLog;
+}
+
+function buildMessageWithInitTime(initTime, endTime, messageToLog, separator, message) {
+  var _endTime = new Date().getTime()
+  var diference = _endTime - initTime
+  if (typeof endTime !== 'undefined' && endTime !== null) {
+    var _guid = endTime
+    messageToLog =
+      `[${diference}][Guid:${_guid}]${separator}${message}`
+  } else {
+    messageToLog =
+      `[${diference}]${separator}${message}`
+  }
+  return messageToLog
+}
+
+function buildMessageWithoutTime(initTime, messageToLog, separator, message) {
+  if (typeof initTime !== 'undefined' && initTime !== null) {
+    var _guid = initTime
+    messageToLog = `[Guid:${_guid}]${separator}${message}`
+  } else
+    messageToLog = separator + message
+  return messageToLog
 }
